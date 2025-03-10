59fa385a27513ac57dd7700bef2a1cdae1a0f6c5
Harry Foster <Fosterh3@cardiff.ac.uk>
2025-03-10 14:34:55 +0000

    Initial Commit
    Initial commit. Still needs additional strategies, some bugfixes, and a readme.

---
+++ Axelrod_Sim.py
@@ -0,0 +1,66 @@
+import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt
+import Strategy as st
+
+
+class Player:
+    def __init__(self, Strat, Name):
+        self.Response = Strat
+        self.Total_Points = 0
+        self.Name = Name
+        self.Times_Player = 0
+
+def Get_Players():
+    #Creates a player for each strategy. Returns list of these players
+    Strats = st.Get_All_Strats()
+    Names = st.Get_All_Names()
+    Players = []
+
+    for A in range(len(Strats)):
+        Players.append(Player(Strats[A], Names[A]))
+    return(Players)
+
+def Axelrod_Single(Months, Player1, Player2, Payoff_Matrix, test_count):
+    #Plays a single run of the Axelrod game (repeated)
+    P1_Play_Hist = []
+    P2_Play_Hist = []
+    P1_Sum = 0
+    P2_Sum = 0
+    for x in range(Months):
+        #print(x)
+        P1Resp = Player1.Response(P2_Play_Hist)
+        P2Resp = Player2.Response(P1_Play_Hist)  #Temporary storage so that they will play simultaneously
+        P1_Play_Hist.append(P1Resp)
+        P2_Play_Hist.append(P2Resp)
+        Player1.Total_Points += (Payoff_Matrix[P1_Play_Hist[x]])[P2_Play_Hist[x]]
+        P1_Sum += (Payoff_Matrix[P1_Play_Hist[x]])[P2_Play_Hist[x]]
+        Player2.Total_Points += (Payoff_Matrix[P2_Play_Hist[x]])[P1_Play_Hist[x]]
+        P2_Sum += (Payoff_Matrix[P2_Play_Hist[x]])[P1_Play_Hist[x]]
+        if P1_Play_Hist[x] == 2:
+            break
+        elif P2_Play_Hist[x] == 2:
+            break
+    return([P1_Sum, P2_Sum, Player1.Name, Player2.Name])
+    
+def Full_Game(Time, Payoff_Matrix):
+    #Plays the entire game
+    m = 0
+    Players = Get_Players()
+    Final_Profit = {}
+    H2H = []
+    for i in Players:
+        for j in Players:
+            Versus_Sum = Axelrod_Single(Time, i, j, Payoff_Matrix, m)
+            H2H.append(Versus_Sum)
+    for A in Players:
+        Final_Profit[A.Name] = A.Total_Points
+    Final_Profit = dict(sorted(Final_Profit.items(), key=lambda item: item[1], reverse=True))
+    return(Final_Profit, H2H)
+
+def Show_Player_Scores(Payoff_Matrix):
+    #displays the scores of each group.
+    Sim_Results = Full_Game(Payoff_Matrix)
+    for (x,y) in Sim_Results[0].items():
+        print(x, y)
+    print(Sim_Results[1])
---
+++ Strategy.py
@@ -0,0 +1,146 @@
+import numpy as np
+from numpy import random
+
+def Always_Coop(Play_Hist): #Always Cooperates
+    return(0)
+def Always_Report(Play_Hist):#Reports turn 1 (Limiting strategy)
+    return(2)
+def Always_Defect(Play_Hist):#Always Defects
+    return(1)
+def Tit_For_Tat(Play_Hist): #Cooperates until opponent defects, then copies their last move each round
+    if len(Play_Hist) == 0:
+        return(0)
+    elif Play_Hist[len(Play_Hist) - 1] == 1:
+        return(1)
+    else:
+        return(0)
+def Two_Tit_For_Tat(Play_Hist): #Cooperates, defects twice if opponent defects once
+    if len(Play_Hist) == 0:
+        return(0)
+    elif Play_Hist[len(Play_Hist) - 1] == 1:
+        return(1)
+    elif Play_Hist[len(Play_Hist) - 2] == 1:
+        return(1)
+    else:
+        return(0)
+
+
+def Three_Month(Play_Hist): #Coop -> Defect -> report
+    if len(Play_Hist) == 0:
+        return(0)
+    elif len(Play_Hist) == 1:
+        return(1)
+    elif len(Play_Hist) == 2:
+        return(2)
+
+def Quick_Profit(Play_Hist): #Defect -> report
+    if len(Play_Hist) == 0:
+        return(1)
+    elif len(Play_Hist) == 1:
+        return(2)
+    
+def Two_Tit(Play_Hist): #Defects if opponent defects twice in a row
+    if len(Play_Hist) == 0:
+        return(0)
+    elif Play_Hist[len(Play_Hist) - 1] == 1:
+        if Play_Hist[len(Play_Hist) - 2] == 1:
+            return(1)
+    else:
+        return(0)
+
+def Giga_Grudger(Play_Hist): #If opponent defects, report
+    if len(Play_Hist) < 2:
+        return(0)
+    elif Play_Hist[len(Play_Hist) - 1] == 2:
+        return(2)
+    else:
+        return(0)
+
+def Wierd(Play_Hist): #Play randomly until turn 8 when it reports for no reason
+    if len(Play_Hist) == 7:
+        return(2)
+    else:
+        return(random.choice([0, 1], p=(0.5, 0.5), size=(1))[0])
+        
+    
+def Tit_For_War(Play_Hist): #If opponent defects, defect and then report
+    if len(Play_Hist) <= 2:
+        return(0)
+    elif Play_Hist[len(Play_Hist)-2] == 1:
+        return(2)
+    elif Play_Hist[len(Play_Hist)-1] == 1:
+        return(1)
+    else:
+        return(0)
+    
+def Grudge(Play_Hist): #If opponent defects, defect for rest of the game
+    if 1 in Play_Hist:
+        return(1)
+    else:
+        return(0)
+
+def Fool_Me_Once(Play_Hist): #If opponent has defected twice this game, report
+    k = 0
+    for n in Play_Hist:
+        if n == 1:
+            k += 1
+    if k >= 2:
+        return(2)
+    else:
+        return(0)
+
+def Patient_Grudger(Play_Hist): #If opponent has defected twice or more this game, defect for the rest of the game
+    k = 0
+    for n in Play_Hist:
+        if n == 1:
+            k += 1
+    if k >= 2:
+        return(1)
+    else:
+        return(0)
+
+
+def Lose_Your_Mind(Play_Hist): #Cooperate until turn 10, then defect twice and report on the final round
+    if len(Play_Hist) == 9:
+        return(1)
+    elif len(Play_Hist) == 10:
+        return(1)
+    elif len(Play_Hist) == 11:
+        return(2)
+    else:
+        return(0)
+    
+def Advantage(Play_Hist): #Be nice on round 1 and every 3rd round. Otherwise, defect
+    if len(Play_Hist)%3 == 0:
+        return(0)
+    else:
+        return(1)
+    
+def Shameful(Play_Hist): #Defect until turn 5. Afterwards, report if they are being nice, report if they are being nasty
+    Times_Defected = 0
+    Times_Nice = 0
+    for i in Play_Hist:
+        if i == 0:
+            Times_Nice += 1
+        else:
+            Times_Defected += 1
+    if len(Play_Hist) >= 5:
+        if Times_Defected > Times_Nice:
+            return(2)
+        else:
+            return(1)
+    else:
+        return(1)
+    
+
+def Get_All_Strats():
+    #Returns an array of all strategy functions
+    return([Always_Coop, Always_Report, Always_Defect, Tit_For_Tat, Two_Tit_For_Tat, Three_Month,
+           Quick_Profit, Giga_Grudger, Wierd, Tit_For_War, Grudge, Fool_Me_Once, Patient_Grudger, Lose_Your_Mind, Advantage,
+           Shameful])
+
+def Get_All_Names():
+    #Returns an array of all strategy names
+    return(["Always Coop", "Always Report", "Always Defect", "Tit For Tat", "Two Tits For a Tat", "Three Month Plan", "Quick Profit",
+            "Giga Grudger", "Wierd", "Tit For Big Tat", "Grudger", "Fool Me Once", "Patient Grudger", "9 Month Plan",
+            "Nice Thrice a Year", "Shameful"])
